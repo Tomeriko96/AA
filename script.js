c4c57c06a15e47603d73fc166d3d1b9642163645
Tomer Iwan <IwanTomer@hotmail.com>
2018-06-18 14:14:15 +0200

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+(function() {
+
+	var preload = document.getElementById("preload");
+	var loading = 0;
+	var id = setInterval(frame, 64);
+
+	function frame() {
+		if(loading == 70) {
+			clearInterval(id);
+			$('#preload').remove();
+		} else {
+			loading = loading + 1;
+			if(loading == 60) {
+				preload.style.animation = "fadeout 1s ease";
+			}
+		}
+	}
+})();
