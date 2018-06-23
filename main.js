f7fcd2ac5c5595d5884a9ed85ff0059ed4343e08
atilla50 <atilla50@users.noreply.github.com>
2018-06-23 14:54:14 +0200

    S H I S H O (X) O H S I H S

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 function contact() {
 	window.open("/contact/index.html", "_self")
 };
+
 var accordions = document.getElementsByClassName("accordion");
+var accordions = document.getElementsByClassName("accordiontest");
 
 for (var i = 0; i < accordions.length; i++) {
   accordions[i].onclick = function() {
+    this.classList.toggle('is-open');
+
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      // accordion is currently open, so close it
+      content.style.maxHeight = null;
+    } else {
+      // accordion is currently closed, so open it
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+  accordiontest[i].onclick = function() {
     this.classList.toggle('is-open');
 
     var content = this.nextElementSibling;
