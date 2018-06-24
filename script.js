077f786e047ea53c1aa2c6bf3a1c13884b20b885
atilla50 <atilla50@users.noreply.github.com>
2018-06-24 16:17:17 +0200

    Comments + css clean-up

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 	var preload = document.getElementById("preload");
 	var loading = 0;
 	var id = setInterval(frame, 48);
-
+	//Redirects User to the Home page of the website when loading reaches 30.
 	function frame() {
 		if(loading == 30) {
 			clearInterval(id);
@@ -11,6 +11,7 @@
 		} else {
 			loading = loading + 1;
 			if(loading == 20) {
+				//Redirecting starts when loading reaches 20 by slowly fading out.
 				preload.style.animation = "fadeout 1s ease";
 			}
 		}
