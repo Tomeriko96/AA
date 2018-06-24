4373b268940329cfb22cc9933830529c43e4f76c
atilla50 <atilla50@users.noreply.github.com>
2018-06-24 16:37:20 +0200

    Cleanup + comment

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-function contact() {
-	window.open("/contact/index.html", "_self")
-};
-
+//+ and - for when the navbar + FAQ accordions are openend and closed.
 var accordions = document.getElementsByClassName("accordion");
 var accordiontest = document.getElementsByClassName("accordiontest");
 
@@ -33,43 +30,3 @@ for (var i = 0; i < accordiontest.length; i++) {
     }
   }
 }
-
-
-$(document).ready(function() {
-    var previousScroll = 0,
-        headerOrgOffset = $('#site-header').offset().top;
-
-    $('#site-header').height($('#wrapper').height());
-
-	// hide #back-top first
-	$("#up").hide();
-
-    $(window).scroll(function() {
-        var currentScroll = $(this).scrollTop();
-		//console.log(currentScroll + " and " + previousScroll + " and " + headerOrgOffset);
-        if ($(this).scrollTop() > 100) {
-				$('#up').fadeIn();
-			} else {
-				$('#up').fadeOut();
-			}
-
-		if(currentScroll > headerOrgOffset) {
-            if (currentScroll > previousScroll) {
-                $('#site-header').fadeOut();
-            } else {
-                $('#site-header').fadeIn(300);
-                $('#site-header').addClass('fixed');
-            }
-        } else {
-             $('#header').removeClass('fixed');
-        }
-        previousScroll = currentScroll;
-		});
-
-		// scroll body to 0px on click
-		$('#up').click(function () {
-			$('body,html').animate({
-			scrollTop: 0}, 800);
-			return false;
-		});
-	});
