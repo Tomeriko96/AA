5858ba64af2ad12b768126dffa120223aaf9cfba
atilla50 <atilla50@users.noreply.github.com>
2018-06-24 16:44:01 +0200

    Text Re-indented

--- main.js
+++ main.js
@@ -3,30 +3,30 @@ var accordions = document.getElementsByClassName("accordion");
 var accordiontest = document.getElementsByClassName("accordiontest");
 
 for (var i = 0; i < accordions.length; i++) {
-  accordions[i].onclick = function() {
-    this.classList.toggle('is-open');
-
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      // accordion is currently open, so close it
-      content.style.maxHeight = null;
-    } else {
-      // accordion is currently closed, so open it
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-}
+	accordions[i].onclick = function() {
+		this.classList.toggle('is-open');
+		
+		var content = this.nextElementSibling;
+		if (content.style.maxHeight) {
+			// accordion is currently open, so close it
+			content.style.maxHeight = null;
+			} else {
+			// accordion is currently closed, so open it
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	}
 }
 for (var i = 0; i < accordiontest.length; i++) {
-  accordiontest[i].onclick = function() {
-    this.classList.toggle('is-open');
-
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      // accordion is currently open, so close it
-      content.style.maxHeight = null;
-    } else {
-      // accordion is currently closed, so open it
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  }
+	accordiontest[i].onclick = function() {
+		this.classList.toggle('is-open');
+		
+		var content = this.nextElementSibling;
+		if (content.style.maxHeight) {
+			// accordion is currently open, so close it
+			content.style.maxHeight = null;
+			} else {
+			// accordion is currently closed, so open it
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	}
 }
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-
+	
 	var preload = document.getElementById("preload");
 	var loading = 0;
 	var id = setInterval(frame, 48);
@@ -8,7 +8,7 @@
 		if(loading == 30) {
 			clearInterval(id);
 			window.open("index.html", "_self");
-		} else {
+			} else {
 			loading = loading + 1;
 			if(loading == 20) {
 				//Redirecting starts when loading reaches 20 by slowly fading out.
